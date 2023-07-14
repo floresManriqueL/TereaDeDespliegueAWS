e52d617ce25f01aa5a9ce73e03876876a9d8d745
Erinda <Erlinda@DESKTOP-DSULF2G.com>
2023-07-14 00:03:52 -0600

    commint con cluster la tarea2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Despliegue Continuo</h1>
+    <h3>Erik Manrique Flores Lara</h3>
+</body>
+</html>
